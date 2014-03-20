03ced27bf84e20bfcf666607a7fafb4c2e4ae01d
Foxboron <mcfoxax@gmail.com>
2014-03-20 17:06:11 +0100

    somethingsomething

--- HyREPL/workarounds.py
+++ HyREPL/workarounds.py
@@ -2,12 +2,14 @@
 
 def work_around_it(session, sessions, msg):
     hints[msg["code"]](session, sessions, msg)
+    return
 
 
 def work_around_fireplace(session, sessions, msg):
     session.write({"out": "success"})
     session.write({"value": "::/home/fox/.lein/self-installs/leiningen-2.3.4-standalone.jar", "ns": "Hy"})
     session.write({"status": ["done"]})
+    return
 
 
 
