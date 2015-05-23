f78f0cf459e957d0cd9dbfe53600ca61ffdf2f7c
Gregor Best <gbe@unobtanium.de>
2015-05-23 14:42:59 +0200

    Repair unit tests
    We're using a UnixStreamServer for easier startup/shutdown.

--- tests/test.py
+++ tests/test.py
@@ -4,46 +4,82 @@ import socket
 import threading
 import queue
 import sys
+import os
+
+from socketserver import ThreadingMixIn, UnixStreamServer
 
 from HyREPL.bencode import encode, decode, decode_multiple
-from main import start_server
+from main import ReplRequestHandler
 
-ip = "localhost"
-port = 1337
+sock = "/tmp/HyREPL-test"
+class TestServer(ThreadingMixIn, UnixStreamServer): pass
+def start_server():
+    os.unlink(sock)
+    s = TestServer(sock, ReplRequestHandler)
+    t = threading.Thread(target=s.serve_forever)
+    t.daemon = True
+    t.start()
+    def stop():
+        s.shutdown()
+        t.join()
+    return stop
+
+def soc_send(message, return_reply=True):
+    s = socket.socket(family=socket.AF_UNIX)
+    s.connect(sock)
+    s.sendall(encode(message))
+    if not return_reply:
+        s.close()
+        return []
+    s.setblocking(False)
+
+    ret = []
+    buf = b""
+    while True:
+        try:
+            resp = s.recv(1)
+            buf += resp
+        except BlockingIOError:
+            pass
+        try:
+            response, rest = decode(buf)
+        except:
+            continue
+        buf = rest
+
+        ret.append(response)
+        if "done" in response.get("status", []):
+            break
+    s.close()
+    return ret
 
 def test_bencode():
     d = { "foo": 42, "spam": [1, 2, 'a'] }
-
-    print(decode(b"d5:value1:47:session36:31594b80-7f2e-4915-9969-f1127d562cc42:ns2:Hyed6:statusl4:donee7:session36:31594b80-7f2e-4915-9969-f1127d562cc4e"))
     assert d == decode(encode(d))[0]
 
-def soc_send(message, return_reply=True):
-    reply = socket.create_connection((ip, port))
-    read_from = reply.makefile('rw')
-    msg = encode(message)
-    ret = []
-    reply.sendall(msg)
-    if return_reply:
-        while True:
-            response = read_from.read(2048)
-            if response:
-                [ret.append(l) for l in decode_multiple(bytes(response, 'utf-8'))]
-            if "done" in response and "status" in response:
-                break
-            reply.close()
-    return ret
-
+    d = decode_multiple(b"d5:value1:47:session36:31594b80-7f2e-4915-9969-f1127d562cc42:ns2:Hye" +\
+            b"d6:statusl4:donee7:session36:31594b80-7f2e-4915-9969-f1127d562cc4e")
+    assert len(d) == 2
+    assert isinstance(d[0], dict)
+    assert isinstance(d[1], dict)
+    assert d[0]["value"] == "4"
+    assert d[0]["ns"] == "Hy"
+    assert isinstance(d[1]["status"], list)
+    assert len(d[1]["status"]) == 1
+    assert d[1]["status"][0] == "done"
 
 def test_code_eval():
     """
+    simple eval
     Example output from the server:
         [{'session': '0361c419-ef89-4a86-ae1a-48388be56041', 'ns': 'Hy', 'value': '4'}, 
          {'status': ['done'], 'session': '0361c419-ef89-4a86-ae1a-48388be56041'}]
     """
-
+    stop = start_server()
     code = {"op": "eval", "code": "(+ 2 2)"}
     ret = soc_send(code)
     value, status = ret
+    stop()
 
     assert len(ret) == 2
     assert value["value"] == '4'
@@ -53,15 +89,18 @@ def test_code_eval():
 
 def test_stdout_eval():
     """
+    stdout eval
     Example output from the server:
         [{'session': '2d6b48d8-4a3e-49a6-9131-3321a11f70d4', 'ns': 'Hy', 'value': 'None'}, 
          {'session': '2d6b48d8-4a3e-49a6-9131-3321a11f70d4', 'out': 'Hello World\n'}, 
          {'status': ['done'], 'session': '2d6b48d8-4a3e-49a6-9131-3321a11f70d4'}]
     """
 
+    stop = start_server()
     code = {"op": "eval", "code": '(print "Hello World")'}
     ret = soc_send(code)
     value, out, status = ret
+    stop()
 
     assert len(ret) == 3
     assert value["value"] == 'None'
@@ -74,10 +113,10 @@ def stdin_send(code, my_queue):
     ret = soc_send(code)
     my_queue.put(ret)
 
-
 def test_stdin_eval():
-
-    """The current implementation will send all the responses back
+    """
+    stdin eval
+    The current implementation will send all the responses back
     into the first thread which dispatched the (def...), so we throw
     it into a thread and add a Queue to get it.
     Bad hack. But it works.
@@ -88,16 +127,16 @@ def test_stdin_eval():
          {'status': ['done'], 'session': 'ec100813-8e76-4d69-9116-6460c1db4428'}]
     """
 
+    stop = start_server()
+
     my_queue = queue.Queue()
 
-
     code = {"op": "eval", "code": '(def a (input))'}
-    t = threading.Thread(target=stdin_send, args=(code,my_queue))
+    t = threading.Thread(target=stdin_send, args=(code, my_queue))
     t.start()
 
     # Might encounter a race condition where 
     # we send stdin before we eval (input)
-    import time
     time.sleep(0.5)
 
     code = {"op": "stdin", "value": "test"}
@@ -107,6 +146,7 @@ def test_stdin_eval():
     ret = my_queue.get()
 
     input_request, value, status = ret
+    stop()
 
     assert len(ret) == 3
     assert value["value"] == 'test'
